b15ca538c3dafae15d6c090458628ab93e55400d
Bright Jonathan <brightjonathan000@gmail.com>
2023-12-21 19:13:26 +0100

    finished working on register

--- client-app/src/Pages/Auth/Register.jsx
+++ client-app/src/Pages/Auth/Register.jsx
@@ -4,6 +4,9 @@ import {AiFillEyeInvisible, AiFillEye} from 'react-icons/ai';
 import Card from '../../Components/Card/Card';
 import RegisterImg from '../../assets/register.gif'
 import { Link } from 'react-router-dom';
+import { useFormik } from 'formik';
+import { basicSchema } from './Scheme/SchemeIndex';
+
 
 const Register = () => {
 
@@ -18,6 +21,34 @@ const Register = () => {
     setConfirmPasswordEye(!confirmPasswordEye)
   }
 
+//handles submit in react
+const onSubmit = async ({username, email, password}, actions)=>{
+      //api will be here
+      try {
+        //console.log({username, email, password});
+      } catch (error) {
+        console.log(error);
+      }
+
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      actions.resetForm();
+}
+ 
+
+    //de-structuring the property of useFormik()
+    const {values, handleBlur, isSubmitting, touched, errors, handleChange, handleSubmit} = useFormik({
+      initialValues: {
+         username: '',
+         email: '',
+         password: '',
+         confirmPassword: "",
+      },
+      
+      //validation
+      validationSchema: basicSchema,
+      onSubmit
+    });
+
 
   return (
     <>
@@ -28,79 +59,79 @@ const Register = () => {
         <div className={styles.form}>
            <h2>Register to chatty app</h2>
            
-           <form autoComplete='off'>
+           <form autoComplete='off' onSubmit={handleSubmit}>
 
            <input
-            // value={values.fullname}
-            // onChange={handleChange}
-            // onBlur={handleBlur}
-            id='fullname' 
+            value={values.username}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            id='username' 
             type="text" 
             placeholder='Enter you name, must be unique' 
             required
-            // className={errors.fullname && touched.fullname ? 'border-[#3a0303]' : ''}
+            className={errors.username && touched.username ? 'border-[red]' : ''}
             />
-            {/* {errors.fullname && touched.fullname && <p className='error'>{errors.fullname}</p>} */}
+            {errors.username && touched.username && <p className='text-[red]'>{errors.username}</p>}
 
             <input
-            // value={values.email}
-            // onChange={handleChange}
-            // onBlur={handleBlur}
+            value={values.email}
+            onChange={handleChange}
+            onBlur={handleBlur}
             id='email' 
             type="email" 
             placeholder='email' 
             required
-            // className={errors.email && touched.email ? 'border-[#3a0303]' : ''}
+            className={errors.email && touched.email ? 'text-[red]' : ''}
             />
-            {/* {errors.email && touched.email && <p className='error'>{errors.email}</p>} */}
+            {errors.email && touched.email && <p className='text-[red]'>{errors.email}</p>}
 
             <div className='my-2 w-full relative'>
             <input 
-            // value={values.password}
-            // onChange={handleChange}
-            // onBlur={handleBlur}
+            value={values.password}
+            onChange={handleChange}
+            onBlur={handleBlur}
             id='password'
             type={(passwordEye === false) ? 'password' : 'text'}
             placeholder='password' 
             required
-            // className={errors.password && touched.password ? 'input-error' : ''}
+            className={errors.password && touched.password ? 'input-error' : ''}
             />
             <div className='absolute right-2 top-6'>
             {(passwordEye === false) ? <AiFillEyeInvisible size={20} onClick={handlePasswordEye} className='text-gray-400'/> : <AiFillEye size={20} onClick={handlePasswordEye} className='text-gray-400'/>}
             </div>
             </div>
-            {/* {errors.password && touched.password && <p className='error'>{errors.password}</p>} */}
+            {errors.password && touched.password && <p className='text-[red]'>{errors.password}</p>}
             
             <div className=' my-2 w-full relative'>
             <input 
-            // value={values.confirmPassword}
-            // onChange={handleChange}
-            //onBlur={handleBlur}
+            value={values.confirmPassword}
+            onChange={handleChange}
+            onBlur={handleBlur}
             id='confirmPassword' 
             type={(confirmPasswordEye === false) ? 'password' : 'text'} 
             placeholder='Confirm password' 
             required
-            // className={errors.confirmPassword && touched.confirmPassword ? 'input-error' : ''}
+            className={errors.confirmPassword && touched.confirmPassword ? 'input-error' : ''}
             />
             <div className='absolute right-2 top-6'>
             {(confirmPasswordEye === false) ? <AiFillEyeInvisible size={20} onClick={handleConfirmPasswordEye} className='text-gray-400'/> : <AiFillEye size={20} onClick={handleConfirmPasswordEye} className='text-gray-400'/>}
             </div>
             </div>
-            {/* {errors.confirmPassword && touched.confirmPassword && <p className='error'>{errors.confirmPassword}</p>} */}
+            {errors.confirmPassword && touched.confirmPassword && <p className='text-[red]'>{errors.confirmPassword}</p>}
              
             <button 
             type='submit' 
-            //disabled={isSubmitting}  
-            className='--btn --btn-primary --btn-block '>Register</button>
+            disabled={isSubmitting}  
+            className='--btn --btn-primary --btn-block'>Register</button>
            </form>
             <span className={styles.register}>
                 <p>Already have an account?</p> 
                 <Link to='/login'> Login</Link>
             </span>
 
-            {/* <span className='m-5 text-center'> <p> By signing up you accept our <Link to='/terms-and-conditions' className='underline'> terms and conditions </Link> 
+            <span className='m-5 text-center'> <p> By signing up you accept our <Link to='/terms-and-conditions' className='underline'> terms and conditions </Link> 
             <Link to='/privacy-policy' className='underline'> & privacy policy </Link> 
-            </p> </span> */}
+            </p> </span>
         </div>
         </Card>
         <div className={styles.img}>
--- client-app/src/main.jsx
+++ client-app/src/main.jsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter as Router } from "react-router-dom";
+import {persistor, store} from './Redux/Store.js'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <Router>
+        <PersistGate loading={null} persistor={persistor}>
         <App/>
+        </PersistGate>
     </Router>
-  </React.StrictMode>,
+  </Provider>,
 )
