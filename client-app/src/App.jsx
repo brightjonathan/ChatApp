9f20bbd568d6cc5517c2a1451aa553b135a15f5f
Bright Jonathan <brightjonathan000@gmail.com>
2023-12-25 13:43:40 +0100

    working on the signup func.

--- client-app/src/App.jsx
+++ client-app/src/App.jsx
@@ -8,6 +8,7 @@ import Login from './Pages/Auth/Login';
 import Homepage from './Pages/Home/Homepage';
 import AppChat from './Pages/Chatbox/AppChat';
 import Private from './Components/PrivateRoutes/Private';
+import NotFound from './Pages/PageNotFound/NotFound';
 
 
 
@@ -20,6 +21,7 @@ const App = () => {
             <Route path="/" element={<Homepage/>} />
             <Route path="/register" element={<Register/>} />
             <Route path="/login" element={<Login/>} />
+            <Route path='*' element={<NotFound/>} />
             <Route element={<Private/>}>
             <Route path="/chat" element={<AppChat />} />
             </Route>
--- client-app/src/Pages/Home/Homepage.jsx
+++ client-app/src/Pages/Home/Homepage.jsx
@@ -2,49 +2,51 @@ import React from 'react';
 import './Home.scss';
 import { Link } from 'react-router-dom';
 import hero from '../../assets/chatHero.png'
+import { useSelector } from 'react-redux';
 
 
 const Homepage = () => {
+
+  const {currentUser} = useSelector(state => state.user);
+
+
   return (
     <div className="home">
     <nav className='container --flex-between'>
     <div className="logo">
         {/* <img src={logo} alt="logo" className='w-[9vh] h-[9vh] lg:w-[15vh] lg:h-[15vh]' /> */}
+        <h2 className='text-[green]'>Chatty app</h2>
       </div>
 
       <ul className="home-links">
 
-        {/* {!currentUser ? <li> <Link to="/register">Register</Link> </li> : null} */}
-
-        {/* {currentUser && <h2 className='text-[white] text-[15px]'> {greeting}, {currentUser.username} </h2>} */}
+        {!currentUser ? <li> <Link to="/register" >Register</Link> </li> : null }
 
           <li>
             <button className="--btn --btn-primary">
-              <Link to="/dashboard">Dashboard</Link>
+              <Link to="/chat" >chat with friends</Link>
             </button>
           </li>
 
-          {/* {currentUser && <Link to={'/edit-profile'}><img src={currentUser.photo} className='rounded-[50%] h-7 w-7 object-cover' alt='profile' /></Link>} */}
-       
        </ul>
     </nav>
 
     {/* hero section */}
     <section className="container hero">
       <div className="hero-text">
-      <h2>e-ventory & Stock Management Solution for business</h2> 
-      <p>
-         our e-ventory system control and manage products in the warehouse in
-          real time and integrated to make it easier to develop your business.
+      <h2> Instant Communication for Seamless Connections</h2> 
+      <p className='font-normal'>
+       Chatty app is an instant messaging platform that enables users to exchange text messages seamlessly.
+       Step into the world of Chatty, where seamless communication is at your fingertips! This innovative messaging app re-defines the way users connect by offering instant text messaging. Whether you're sharing quick updates or engaging in meaningful conversations, Chatty ensures a smooth and efficient exchange of thoughts and ideas. Join the conversation and experience the convenience of instant communication with the Chatty app!
         </p><div className="hero-buttons">
           <button className="--btn --btn-secondary">
-            <Link to="/dashboard">Get started for Free</Link>
+            <Link to="/chat">Get started for Free</Link>
           </button>
         </div>
         <div className="--flex-start">
-          <NumberText num="14K" text="Brand Owners"/>
-          <NumberText num="23K" text="Active Users" />
-          <NumberText num="500+" text="Partners" />
+          <NumberText num="3K" text="Owners"/>
+          <NumberText num="2K" text=" Users" />
+          <NumberText num="50+" text="Partners" />
         </div>
       </div>
       <div className="hero-image">
